1fa6688ce35dffbcd0b425a95444f1ef8e5c542c
Dan Zaharia <zaharia.danny@gmail.com>
2016-12-27 15:41:05 -0600

    add a slight zoom to the focus animation to make it more realistic

--- public/scripts/hongkong-title.js
+++ public/scripts/hongkong-title.js
@@ -1,20 +1,22 @@
 function createFocusRing () {
-  console.log('creating focus ring');
-
-  let backImage = document.querySelector('.hong-kong__intro-card-background-image');
-  let title = document.querySelector('.hong-kong__intro-card-container');
+  let hongKongContainer = document.querySelector('.location-container--hong-kong');
 
   window.addEventListener('mousemove', (event) => {
     let mouseX = ((event.clientX / window.innerWidth) * 100).toFixed(0);
-    focusRing(mouseX, backImage, title);
+    focusRing(mouseX, hongKongContainer);
   });
 
-  function focusRing(mouseX, backImage, title) {
+  function focusRing(mouseX, container) {
+    let backImage = container.querySelector('.hong-kong__intro-card-background-image');
+    let title = container.querySelector('.hong-kong__intro-card-container');
     let titleBlur = (Math.abs(50 - mouseX)) / 10;
-    let backImageBlur = ((-.0256 * Math.pow(mouseX, 2)) + (2.56 * mouseX) - 48) / 5;
+    let backImageBlur = (Math.abs(25 - mouseX)) / 10;
+    // let backImageBlur = ((-.0256 * Math.pow(mouseX, 2)) + (2.56 * mouseX) - 48) / 5;
 
     title.style.filter = `blur(${titleBlur}px)`;
     backImage.style.filter =  `blur(${backImageBlur}px)`;
+    document.querySelector('.location-container--hong-kong').style.transform = `scale(${1.025 - (mouseX / 4000)})`;
+
   }
 }
 
