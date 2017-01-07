baeee27db13626c26293b91209a171275a80c70e
Dan Zaharia <zaharia.danny@gmail.com>
2017-01-07 16:18:07 -0600

    created a radial, rather than linear, interaction for the focus ring

--- public/scripts/hongkong-title.js
+++ public/scripts/hongkong-title.js
@@ -1,22 +1,27 @@
 function createFocusRing () {
   let hongKongContainer = document.querySelector('.location-container--hong-kong');
 
-  window.addEventListener('mousemove', (event) => {
-    let mouseX = ((event.clientX / window.innerWidth) * 100).toFixed(0);
-    focusRing(mouseX, hongKongContainer);
+  hongKongContainer.addEventListener('mousemove', (event) => {
+    // let mouseX = ((event.clientX / window.innerWidth) * 100).toFixed(0);
+    // let mouseY = ((event.clientY / window.innerHeight) * 100).toFixed(0);
+    focusRing(event.clientX, event.clientY + document.body.scrollTop, hongKongContainer);
   });
 
-  function focusRing(mouseX, container) {
+  function focusRing(mouseX, mouseY, container) {
     let backImage = container.querySelector('.hong-kong__intro-card-background-image');
     let title = container.querySelector('.hong-kong__intro-card-container');
-    let titleBlur = (Math.abs(50 - mouseX)) / 10;
-    let backImageBlur = (Math.abs(25 - mouseX)) / 10;
-    // let backImageBlur = ((-.0256 * Math.pow(mouseX, 2)) + (2.56 * mouseX) - 48) / 5;
+    let containerYCenter = (container.getBoundingClientRect().bottom + container.getBoundingClientRect().top) / 2;
+    let containerXCenter = (container.getBoundingClientRect().right + container.getBoundingClientRect().left) / 2;
+    let circleRadius = (container.getBoundingClientRect().right - container.getBoundingClientRect().left) / 2;
+    let mousePercentage = (Math.sqrt(
+      Math.pow(mouseX - containerXCenter, 2) + Math.pow(mouseY - containerYCenter, 2)
+    ) / circleRadius * 100).toFixed(0);
 
+    let backImageBlur = (Math.abs(50 - mousePercentage)) / 10;
+    let titleBlur = (mousePercentage - 20) / 10;
     title.style.filter = `blur(${titleBlur}px)`;
-    backImage.style.filter =  `blur(${backImageBlur}px)`;
-    document.querySelector('.location-container--hong-kong').style.transform = `scale(${1.025 - (mouseX / 4000)})`;
-
+    backImage.style.filter = `blur(${backImageBlur}px)`;
+    document.querySelector('.location-container--hong-kong').style.transform = `scale(${1.025 - (mousePercentage / 4000)})`;
   }
 }
 
