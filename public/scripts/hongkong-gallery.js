278c93572e1d04a1ffc4858e29b18bea659babf3
Dan Zaharia <zaharia.danny@gmail.com>
2017-01-09 21:19:16 -0600

    implement focus on scroll

--- public/scripts/hongkong-gallery.js
+++ public/scripts/hongkong-gallery.js
@@ -1,17 +1,23 @@
 function hkGallerySetup () {
-  let gallerySections = document.querySelectorAll('.hong-kong__gallery-section');
+  let images = document.querySelectorAll('.hong-kong__gallery-section-image>img');
 
-  for (section of gallerySections) {
+  // create stick-up caps in each section's paragraph
+  for (section of document.querySelectorAll('.hong-kong__gallery-section')) {
     let text = section.querySelector('.hong-kong__gallery-section-text');
     var textContent = text.innerText;
-    text.innerHTML = `<span class="hong-kong__gallery-section-text--first-letter">${textContent.substr(0,1)}</span>${textContent.substr(1)}`;
+    text.innerHTML = `<span class="hong-kong__gallery-section-text--stick-up-cap">${textContent.substr(0,1)}</span>${textContent.substr(1)}`;
   }
 
-  window.addEventListener('scroll', scrollFocus);
+  window.addEventListener('scroll', (event) => {
+    let windowCenter = document.body.scrollTop + (window.innerHeight / 2);
 
-  function scrollFocus () {
+    for (let image of images) {
+      let imageCenter = image.getBoundingClientRect().top + (image.offsetHeight / 2);
+      let filterFactor = Math.abs((imageCenter - (window.innerHeight / 2)) / 200).toFixed(4);
 
-  }
+      image.style.filter = `blur(${filterFactor}px)`;\
+    }
+  });
 }
 
 scripts.push(hkGallerySetup);
