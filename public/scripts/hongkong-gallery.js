ec7ff249dcb0b6b66ccaf5a2f21753797bb0e512
Dan Zaharia <zaharia.danny@gmail.com>
2017-01-14 14:52:11 -0600

    only apply blur effect to gallery images within the window bounds

--- public/scripts/hongkong-gallery.js
+++ public/scripts/hongkong-gallery.js
@@ -8,16 +8,23 @@ function hkGallerySetup () {
     text.innerHTML = `<span class="hong-kong__gallery-section-text--stick-up-cap">${textContent.substr(0,1)}</span>${textContent.substr(1)}`;
   }
 
-  window.addEventListener('scroll', (event) => {
-    let windowCenter = document.body.scrollTop + (window.innerHeight / 2);
+  blurImages();
 
+  window.addEventListener('scroll', blurImages);
+
+  function blurImages() {
     for (let image of images) {
-      let imageCenter = image.getBoundingClientRect().top + (image.offsetHeight / 2);
-      let filterFactor = Math.abs((imageCenter - (window.innerHeight / 2)) / 200).toFixed(4);
+      let imageRect = image.getBoundingClientRect();
 
-      image.style.filter = `blur(${filterFactor}px)`;
+      if (imageRect.top < (window.innerHeight) && imageRect.bottom > 0) {
+        let imageCenter = imageRect.top + (image.offsetHeight / 2);
+        let filterFactor = Math.abs((imageCenter - (window.innerHeight / 2)) / 100).toFixed(4);
+
+        image.style.filter = `blur(${filterFactor}px)`;
+        image.style.transform = `scale(${1 - filterFactor / 400})`;
+      }
     }
-  });
+  }
 }
 
 scripts.push(hkGallerySetup);
